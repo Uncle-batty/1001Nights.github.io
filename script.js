436a6ce57ffec541b0ba9a37353cbe8ae25348b3
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2023-07-27 18:43:56 +0200

    New landing pages

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded',  () => {
     
+    setTimeout(function () {
+        var firstImage = document.getElementById("firstImage");
+        var secondImage = document.getElementById("secondImage");
 
+        firstImage.style.left = "-50%";
+        /* Move the first image to the left */
+        /*firstImage.style.opacity = "0";*/
+
+        secondImage.style.left = "50%";
+        /* Move the second image to the center of the container */
+        secondImage.style.opacity = "1";
+    }, 3000);
     // Add click event to the hero section for redirection
     const heroSection = document.getElementById('hero');
     heroSection.addEventListener('click', () => {
